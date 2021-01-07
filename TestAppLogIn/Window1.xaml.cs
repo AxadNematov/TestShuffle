da09d7cb9f6e8f9491c53a62a4fad7f1d6ee7106
AxadNematov <axad.nem4tov@gmail.com>
2021-01-08 01:03:15 +0500

    last changes

--- TestAppLogIn/Window1.xaml.cs
+++ TestAppLogIn/Window1.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,11 +7,13 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Spire.Doc;
+using Spire.Doc.Documents;
+using Spire.Doc.Fields;
 
 namespace TestAppLogIn
 {
@@ -39,6 +42,63 @@ namespace TestAppLogIn
         {
             ButtonOpenMenu.Visibility = Visibility.Visible;
             ButtonCloseMenu.Visibility = Visibility.Collapsed;
+        }
+        public void LoadFile(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.DefaultExt = ".doc";
+            dlg.Filter = "Word documents (.doc)|*.docx";
+            Nullable<bool> result = dlg.ShowDialog();
+            if (result == true)
+            {
+                if (dlg.FileName.Length > 0)
+                {
+                    richTextBox.Document.Blocks.Clear();
+                    //richTextBox.AppendText(dlg.FileName+"\n");
+                    Document document = new Document();
+                    document.LoadFromFile(dlg.FileName);
+                    string text = document.GetText();
+                    int i = 1;
+                    while (text.Length > 0)
+                    {
+                        i++;
+                        int beginningTest = text.IndexOf("" + i.ToString() + ". ");
+                        int endingTest = text.IndexOf("A)");
+                        string test = text.Substring(beginningTest, endingTest - beginningTest);
+                        int beginningV = text.IndexOf("A)");
+                        int endingV = text.IndexOf((i + 1).ToString() + ". ");
+                        if (endingV == -1)
+                        {
+                            endingV = text.Length - 1;
+                        }
+                        string variants = text.Substring(beginningV, endingV - beginningV);
+
+                        text = text.Substring(endingV, text.Length - endingV - 1);
+                        richTextBox.AppendText(test + "\n");
+                        richTextBox.AppendText(variants + "\n");
+
+                    }
+
+                }
+            }
+        }
+        public void Shuffle(object sender, RoutedEventArgs e)
+        {
+        }
+        public void Download(object sender, RoutedEventArgs e)
+        {
+            Document document = new Document();
+            Spire.Doc.Section section = document.AddSection();
+            Paragraph paragraph = section.AddParagraph();
+            TextRange text = paragraph.AppendText("some");
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "DOCX (*.docx)|*.docx";
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                document.SaveToFile(saveFileDialog.FileName);
+            }
         }
     }
 }
