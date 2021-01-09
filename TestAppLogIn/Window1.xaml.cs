10346f6f73cd96334eefce4bfa12e9dccf0becc7
AxadNematov <axad.nem4tov@gmail.com>
2021-01-10 03:37:16 +0500

    ready product

--- TestAppLogIn/Window1.xaml.cs
+++ TestAppLogIn/Window1.xaml.cs
@@ -22,27 +22,58 @@ namespace TestAppLogIn
     /// </summary>
     public partial class Window1 : Window
     {
+        public List<Test> testList = new List<Test>();
+        private int _numValue = 0;
         public Window1()
         {
             InitializeComponent();
+            txtNum.Text = _numValue.ToString();
+        }
+        public int NumValue
+        {
+            get { return _numValue; }
+            set
+            {
+                _numValue = value;
+                txtNum.Text = value.ToString();
+            }
+        }
+        private void cmdUp_Click(object sender, RoutedEventArgs e)
+        {
+            NumValue++;
         }
 
+        private void cmdDown_Click(object sender, RoutedEventArgs e)
+        {
+            NumValue--;
+        }
+
+        private void txtNum_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (txtNum == null)
+            {
+                return;
+            }
+
+            if (!int.TryParse(txtNum.Text, out _numValue))
+                txtNum.Text = _numValue.ToString();
+        }
+        public class Test
+        {
+            public int TestId { get; set; }
+            public string TestText { get; set; }
+            public List<Variant> Variants { get; set; }
+        };
+        public class Variant
+        {
+            public int VariantId { get; set; }
+            public string VariantText { get; set; }
+        };
         private void ButtonLogout_Click(object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown();
         }
 
-        private void ButtonOpenMenu_Click(object sender, RoutedEventArgs e)
-        {
-            ButtonOpenMenu.Visibility = Visibility.Collapsed;
-            ButtonCloseMenu.Visibility = Visibility.Visible;
-        }
-
-        private void ButtonCloseMenu_Click(object sender, RoutedEventArgs e)
-        {
-            ButtonOpenMenu.Visibility = Visibility.Visible;
-            ButtonCloseMenu.Visibility = Visibility.Collapsed;
-        }
         public void LoadFile(object sender, RoutedEventArgs e)
         {
             OpenFileDialog dlg = new OpenFileDialog();
@@ -58,40 +89,162 @@ namespace TestAppLogIn
                     Document document = new Document();
                     document.LoadFromFile(dlg.FileName);
                     string text = document.GetText();
-                    int i = 1;
+                    int i = 0;
                     while (text.Length > 0)
                     {
                         i++;
                         int beginningTest = text.IndexOf("" + i.ToString() + ". ");
-                        int endingTest = text.IndexOf("A)");
-                        string test = text.Substring(beginningTest, endingTest - beginningTest);
-                        int beginningV = text.IndexOf("A)");
-                        int endingV = text.IndexOf((i + 1).ToString() + ". ");
-                        if (endingV == -1)
+                        if(beginningTest!=-1)
                         {
-                            endingV = text.Length - 1;
+                            int endingTest = text.IndexOf("A)");
+                            string test = text.Substring(beginningTest, endingTest - beginningTest);
+
+                            bool space = true;
+                            while (space)
+                            {
+                                char last = test[test.Length - 1];
+                                if (last.ToString() == " " || last.ToString() == "\n" || last.ToString() == "\r")
+                                {
+                                    test = test.Remove(test.Length - 1);
+                                }
+                                else { space = false; }
+                            }
+
+                            int beginningVariantA = text.IndexOf("A)");
+                            int endingVariantA = text.IndexOf("B)");
+                            string variantA = text.Substring(beginningVariantA, endingVariantA - beginningVariantA);
+
+                            int beginningVariantB = text.IndexOf("B)");
+                            int endingVariantB = text.IndexOf("C)");
+                            string variantB = text.Substring(beginningVariantB, endingVariantB - beginningVariantB);
+
+                            int beginningVariantC = text.IndexOf("C)");
+                            int endingVariantC = text.IndexOf("D)");
+                            string variantC = text.Substring(beginningVariantC, endingVariantC - beginningVariantC);
+
+                            int beginningVariantD = text.IndexOf("D)");
+                            int endingVariantD = text.IndexOf((i + 1).ToString() + ". ");
+                            
+                            if (endingVariantD == -1)
+                            {
+                                endingVariantD = text.Length - 1;
+                            }
+                            string variantD = text.Substring(beginningVariantD, endingVariantD - beginningVariantD);
+
+                            bool hasSpace = true;
+                            while(hasSpace)
+                            {
+                                char last = variantD[variantD.Length - 1];
+                                if (last.ToString() == " " || last.ToString() == "\n" || last.ToString() == "\r")
+                                {
+                                    variantD = variantD.Remove(variantD.Length - 1);
+                                }
+                                else { hasSpace = false; }
+                            }
+                            variantD = variantD + "     ";
+
+                            List<Variant> listVariants = new List<Variant>();
+                            Variant a = new Variant() { VariantId = 1, VariantText = variantA };
+                            Variant b = new Variant() { VariantId = 2, VariantText = variantB };
+                            Variant c = new Variant() { VariantId = 3, VariantText = variantC };
+                            Variant d = new Variant() { VariantId = 4, VariantText = variantD };
+                            listVariants.Add(a);
+                            listVariants.Add(b);
+                            listVariants.Add(c);
+                            listVariants.Add(d);
+                            Test newTest = new Test()
+                            {
+                                TestId = i,
+                                TestText = test,
+                                Variants = listVariants
+                            };
+
+                            testList.Add(newTest);
+
+                            text = text.Substring(endingVariantD, text.Length - endingVariantD - 1);
+                            richTextBox.AppendText(test+'\n');
+                            richTextBox.AppendText(variantA);
+                            richTextBox.AppendText(variantB);
+                            richTextBox.AppendText(variantC);
+                            richTextBox.AppendText(variantD+'\n');
                         }
-                        string variants = text.Substring(beginningV, endingV - beginningV);
-
-                        text = text.Substring(endingV, text.Length - endingV - 1);
-                        richTextBox.AppendText(test + "\n");
-                        richTextBox.AppendText(variants + "\n");
-
                     }
 
                 }
             }
         }
+       
         public void Shuffle(object sender, RoutedEventArgs e)
         {
+
+            pbStatus.Visibility = Visibility.Visible;
+            richTextBox.Document.Blocks.Clear();
+            int numberOfVariants = 0;
+            for(numberOfVariants=0; numberOfVariants<_numValue; numberOfVariants++)
+            {
+                richTextBox.AppendText("Variant " + (numberOfVariants+1).ToString()+"\n\n");
+                int a = 0;
+                a = testList.Count();
+                var rnd = new Random();
+                var newList = testList.OrderBy(item => rnd.Next());
+                int testNumber = 0;
+                foreach (var item in newList)
+                {
+                    testNumber++;
+                    string text = item.TestText.Substring(item.TestText.IndexOf(". "), item.TestText.Length - item.TestText.IndexOf(". "));
+                    richTextBox.AppendText(testNumber.ToString() + text + "\n");
+                    var newVariants = item.Variants.OrderBy(variant => rnd.Next());
+                    int variantOrder = 0;
+                    foreach (var variant in newVariants)
+                    {
+                        variantOrder++;
+                        string variantText = variant.VariantText.Substring(2, variant.VariantText.Length - 2);
+                        switch (variantOrder)
+                        {
+                            case 1:
+                                variantText = "A)" + variantText;
+                                break;
+                            case 2:
+                                variantText = "B)" + variantText;
+                                break;
+                            case 3:
+                                variantText = "C)" + variantText;
+                                break;
+                            case 4:
+                                variantText = "D)" + variantText;
+                                break;
+                            default:
+                                variantText = "E)" + variantText;
+                                break;
+                        }
+                        richTextBox.AppendText(variantText);
+                    }
+                    richTextBox.AppendText("\n\n");
+                }
+            }
+            pbStatus.IsIndeterminate = false;
         }
         public void Download(object sender, RoutedEventArgs e)
         {
             Document document = new Document();
-            Spire.Doc.Section section = document.AddSection();
-            Paragraph paragraph = section.AddParagraph();
-            TextRange text = paragraph.AppendText("some");
 
+            string fullString = new System.Windows.Documents.TextRange(richTextBox.Document.ContentStart, richTextBox.Document.ContentEnd).Text;
+            for(int i = 0; i<_numValue; i++)
+            {
+                int beginning = fullString.IndexOf("Variant " + (i + 1).ToString());
+                int ending = fullString.IndexOf("Variant " + (i + 2).ToString());
+                if(ending==-1)
+                {
+                    ending = fullString.Length;
+                }
+                string variant = fullString.Substring(beginning, ending - beginning);
+                Section section = document.AddSection();
+
+                Paragraph paragraph = section.AddParagraph();
+                TextRange text = paragraph.AppendText(variant);
+            }
+
+            
             SaveFileDialog saveFileDialog = new SaveFileDialog();
             saveFileDialog.Filter = "DOCX (*.docx)|*.docx";
 
